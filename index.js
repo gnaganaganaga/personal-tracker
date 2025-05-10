3a7126e151b48d96d78b7e47964ed4203d01f64e
gnaganaganaga <g.naganaganaga@gmail.com>
2025-05-10 13:59:24 +0530

    Initial commit

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const session = require('express-session');
 const path = require('path');
 const pg = require('pg');
